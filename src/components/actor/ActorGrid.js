f518575f67b59f34ad8ec139093ffc527f4a1ac4
ChaoticSeas <ariesh43@gmail.com>
2021-12-20 18:18:41 +0530

    Added styled components, added styles for search-cards

--- src/components/actor/ActorGrid.js
+++ src/components/actor/ActorGrid.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { ActorCard } from './ActorCard';
 import IMAGE_NOT_FOUND from '../../images/not-found.png';
+import { FlexGrid } from '../styled';
 
 export const ActorGrid = ({ data }) => {
   return (
-    <div>
+    <FlexGrid>
       {data.map(({ person }) => (
         <ActorCard
           key={person.id}
@@ -16,6 +17,6 @@ export const ActorGrid = ({ data }) => {
           image={[person.image ? person.image.medium : IMAGE_NOT_FOUND]}
         />
       ))}
-    </div>
+    </FlexGrid>
   );
 };
