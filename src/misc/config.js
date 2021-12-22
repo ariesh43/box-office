7dc92c3dac19649c60359442b29da6206420c371
ChaoticSeas <ariesh43@gmail.com>
2021-12-22 22:22:05 +0530

    Updates

--- src/misc/config.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios';
-
-const API_BASE_URL = 'https://api.tvmaze.com';
-export const apiGet = async queryString => {
-  const response = await axios.get(`${API_BASE_URL}${queryString}`);
-  return response.data;
-};
